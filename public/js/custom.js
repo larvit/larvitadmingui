d8ebaf4c1b14943fc0b019fcf5c3f61bec363743
Valter Karlsson <valter@larvit.se>
2015-09-11 15:17:43 +0200

    cookies controlling left nav

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,4 +2,18 @@ $(function() {
 	/* Datepicker begin */
 	$('.data_date').datepicker();
 	/* Datepicker end */
+	/* if desktop AND cookie says so - show left nav */
+	if ($.browser.desktop && Cookies.get('leftNav') === '1') {
+		location.hash = 'main_nav';
+	}
+	/* if click hamburger AND left nav is hidden - set cookie AND show left nav */
+	$('a.open_menu').on('click', function () {
+		Cookies.set('leftNav', '1');
+		/*location.hash = 'main_nav';*/
+	});
+	/* if click hamburger AND left nav is visible - remove cookie AND hide left nav */
+	$('a.close_menu').on('click', function () {
+		Cookies.remove('leftNav');
+		/*location.hash = '';*/
+	});
 });
