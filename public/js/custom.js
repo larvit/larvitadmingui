45d2741e3f1c81b946648995d93b6a65bdc177ef
Valter Karlsson <valter@larvit.se>
2015-09-14 16:00:59 +0200

    starting up sticky bot nav etc

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,19 +1,40 @@
-$(function() {
+/* Win resize handler */
+function resizeHandler () {
+	$('#msg_print').text('dh :: ' + $(document).height() + ', wh :: ' +$(window).height());
+	/* If content height > viewport height - make bottom sub nav sticky */
+	if ($(document).height() >= $(window).height()) {
+		$('.sticky_bot').addClass('sticky');
+		$('.sticky_bot .columns').css('width', $('#main_content').width() + 'px');
+	} else {
+		$('.sticky_bot').removeClass('sticky');
+		$('.sticky_bot .columns').css('width', '');
+	}
+}
+
+$(document).ready(function () {
 	/* Datepicker begin */
 	$('.data_date').datepicker();
-	/* Datepicker end */
-	/* if desktop AND cookie says so - show left nav */
+
+	/* If desktop AND cookie says so - show left nav */
 	if ($(window).width() > 768 && Cookies.get('leftNav') === '1') {
 		location.hash = 'main_nav';
+		//$(window).delay(500).trigger('resize');
 	}
-	/* if click hamburger AND left nav is hidden - set cookie AND show left nav */
+
+	/* If click hamburger AND left nav is hidden - set cookie AND show left nav */
 	$('a.open_menu').on('click', function () {
 		Cookies.set('leftNav', '1');
+		//$(window).delay(500).trigger('resize');
 		/*location.hash = 'main_nav';*/
 	});
-	/* if click hamburger AND left nav is visible - remove cookie AND hide left nav */
+
+	/* If click hamburger AND left nav is visible - remove cookie AND hide left nav */
 	$('a.close_menu').on('click', function () {
 		Cookies.remove('leftNav');
+		//$(window).delay(500).trigger('resize');
 		/*location.hash = '';*/
 	});
 });
+
+/* Bind to window resize */
+$(window).bind('resize', resizeHandler).trigger('resize');
